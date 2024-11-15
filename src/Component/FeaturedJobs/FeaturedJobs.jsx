ae3a259a51f5766ed3644e21c00509a68fb5e4ce
fardinariful <fardinewucse2020@gmail.com>
2024-11-15 19:37:47 +0600

    show all button added with function

--- src/Component/FeaturedJobs/FeaturedJobs.jsx
+++ src/Component/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,7 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
         const [jobs,setjobs]=useState([]);
+        const [joblength,setjonlength]=useState(4);
         useEffect(()=>{
             fetch('jobs.json')
             .then(res=>res.json())
@@ -10,14 +11,18 @@ const FeaturedJobs = () => {
         },[])
     return (
         <div>
-        <div>
+        <div className="space-y-3">
             <h2 className="text-center text-5xl">Featured Jobs:{jobs.length}</h2>
+            <p className="text-[#757575] font-medium">Explore thousands of job opportunities with all the information you need. Its your future</p>
         </div>
         <div className="grid grid-cols-2 gap-4">
             {
-                jobs.map(job =><Job key={job.id} job={job}></Job>)
+                jobs.slice(0,joblength).map(job =><Job key={job.id} job={job}></Job>)
             }
         </div>
+        <div className={joblength===jobs.length ? 'hidden' : ' ' }>
+                    <button className="btn btn-primary text-white w-48 mt-4" onClick={()=>setjonlength(jobs.length)}>Show all</button>
+            </div>
         </div>
     );
 };
--- src/Component/Job/Job.jsx
+++ src/Component/Job/Job.jsx
@@ -1,3 +1,5 @@
+import { CiLocationOn } from "react-icons/ci";
+import { AiOutlineDollar } from "react-icons/ai";
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
@@ -10,22 +12,32 @@ const Job = ({ job }) => {
                         
                     />
                 </figure>
-                <div className="p-4 w-full text-left">
-                    <h2 className="text-lg font-bold">{job_title}</h2>
-                    <p className="text-xl text-gray-600">{company_name}</p>
+                <div className="p-4 w-full text-left space-y-4">
+                    <h2 className="text-2xl font-bold text-[#474747]">{job_title}</h2>
+                    <p className="text-xl  text-[#757575]">{company_name}</p>
                     <div className="flex justify-start items-center mt-2">
                         <button className="text-[#7E90FE] border border-[#9873FF] px-5 py-2 rounded-lg font-semibold mr-4">
-                            Remote
+                            {remote_or_onsite}
                         </button>
-                        <button className="text-[#7E90FE] border border-[#9873FF] px-5 py-2 rounded-lg font-semibold mr-4">
-                            Full Time
+                        <button className="text-[#8186a6] border border-[#9873FF] px-5 py-2 rounded-lg font-semibold mr-4">
+                             {job_type}
                         </button>
+                    </div>
+                    <div className="flex flex-row  gap-5">
+                        <h2 className="flex gap-2 font-semibold"><CiLocationOn className="text-2xl"></CiLocationOn>
+                        {location}
+                        </h2>
+                        <h2 className="flex gap-2 font-semibold"><AiOutlineDollar className="text-2xl"></AiOutlineDollar>
+                        Salary : {salary}
+                        </h2>
                     </div>
                     <div className="mt-4">
                         <button className="btn btn-primary">View Details</button>
                     </div>
                 </div>
+                
             </div>
+            
         </div>
     );
 };
